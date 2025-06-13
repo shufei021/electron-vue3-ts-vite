983b52a35654a3e2e4a9efce5b910d495a7cf072
Shufei <1017981699@qq.com>
2025-06-13 12:04:04 +0800

    feat: init

---
+++ src/renderer/src/env.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="vite/client" />
+import { ElectronAPI } from '@electron-toolkit/preload'
+
+declare global {
+  interface Window {
+    electron: ElectronAPI
+    api: unknown
+  }
+}
